1f0abb1a280bd1c6a3cad7c47b5f2ac9915b39c8
yangjingyuan <yangjingyuan@pwrd.com>
2024-12-20 19:28:44 +0800

    feat: backend init

---
+++ backend/routes/index.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
+
+
+
+// An endpoint which would work with the client code above - it returns
+// the contents of a REST API request to this protected endpoint
+router.get("/session", async (req, res) => {
+  const r = await fetch("https://api.openai.com/v1/realtime/sessions", {
+    method: "POST",
+    headers: {
+      "Authorization": `Bearer ${process.env.OPENAI_API_KEY}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      model: "gpt-4o-realtime-preview-2024-12-17",
+      voice: "verse",
+    }),
+  });
+  const data = await r.json();
+
+  // Send back the JSON we received from the OpenAI REST API
+  res.send(data);
+});
+
+
+
+module.exports = router;
